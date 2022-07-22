2628189c91309bb31d1f5847c30dcececb682fac
srushtimane27 <srushtimane31@gmail.com>
2022-07-22 20:39:53 +0530

    Stack Java Programs

--- src/com/Bridgelabz/Runner.java
+++ src/com/Bridgelabz/Runner.java
@@ -1,2 +1,12 @@
-package com.Bridgelabz;public class Runner {
+package com.Bridgelabz;
+
+public class Runner {
+    public static void main(String[] args) {
+        Stack nums = new Stack();
+        nums.push(56);
+        nums.push(30);
+        nums.push(70);
+
+        nums.show();
+    }
 }
--- src/com/Bridgelabz/Stack.java
+++ src/com/Bridgelabz/Stack.java
@@ -1,2 +1,15 @@
-package com.Bridgelabz;public class Stack {
+package com.Bridgelabz;
+
+public class Stack {
+    int stack[] = new int[3];
+    int top = 0;                                        //push value at top
+    public void push(int data){
+        stack[top] = data;
+        top++;                                          //increse top value
+    }
+    public void show(){
+        for (int n : stack){
+            System.out.print(n + " ");
+        }
+    }
 }
